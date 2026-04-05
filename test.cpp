c8502006906a7572433ee5944a33efe7b0814849
RudrSharma1 <rudrsharma1210@gmail.com>
2026-04-05 10:26:29 +0530

    chore: DEBUG flag for testing move generation

--- test.cpp
+++ test.cpp
@@ -1,9 +1,15 @@
 #include "engine.hpp"
-
+constexpr bool DEBUG = false;
 int main()
 {
     engine::Board board;
     board.print_board();
+
+    if(DEBUG){
+        // to test move gen(temporary)
+        auto moves = board.pseudo_legal_move_gen();
+        std::cout << "Total moves: " << moves.count << '\n';
+    }
 
     return 0;
 }
