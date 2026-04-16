e822932c92564356351d85bd57dda3f637e06476
gradekeyboard-boop <gradekeyboards@gmail.com>
2026-04-16 17:16:05 -0300

    feat: added a basic but working UCI

--- test.cpp
+++ test.cpp
@@ -3,10 +3,11 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include <sstream>
 
 static constexpr bool DEBUG = false;
-static constexpr bool PLAY = true;
-static constexpr bool UCI = false;
+static constexpr bool PLAY = false;
+static constexpr bool UCI = true;
 
 int main()
 {
@@ -133,18 +134,128 @@ int main()
     }
     else if (UCI)
     {
+        int colour{ engine::Colour::white };
+
+        std::unordered_map<int, std::string> index_to_string{ engine::Board::get_index_to_string() };
+        std::unordered_map<std::string, int> string_to_index{ engine::Board::get_string_to_index() };
+        std::unordered_map<char, int> string_to_piece{};
+
+        std::array<char, 7> piece_to_string{ 'x', 'p', 'n', 'b', 'r', 'q', 'k'  };
+        for (int i{}; i < piece_to_string.size(); i++)
+        {
+            string_to_piece[piece_to_string[i]] = i;
+        }
+
         std::string input{};
         while (std::getline(std::cin, input))
         {
-            if (input == "uci")
+            std::istringstream input_stream(input);
+            std::string word{};
+
+            while (input_stream >> word)
             {
-                std::cout << "uciok" << '\n';
-                std::cout << std::flush;
-            }
-            else if (input == "isready")
-            {
-                std::cout << "readyok" << '\n';
-                std::cout << std::flush;
+                if (word == "uci")
+                {
+                    std::cout << "id name Engine" << std::endl;
+                    std::cout << "id author Us" << std::endl;
+                    std::cout << "uciok" << std::endl;
+                }
+                else if (word == "isready")
+                {
+                    std::cout << "readyok" << '\n';
+                    std::cout << std::flush;
+                }
+                else if (word == "position")
+                {
+                    std::string fen_or_moves{};
+                    input_stream >> fen_or_moves;
+
+                    if (fen_or_moves == "fen")
+                    {
+                        // TODO: make a function that converts from fen to board + correct board state
+                        continue;
+                    }
+                    else if (fen_or_moves == "startpos")
+                    {
+                        board = engine::Board();
+                        colour = engine::Colour::white;
+
+                        std::string next_move{};
+                        while (input_stream >> next_move)
+                        {
+                            if (next_move == "moves")
+                            {
+                                continue;
+                            }
+
+                            std::string start_square{};
+                            std::string destination_square{};
+                            char promotion_piece{};
+
+                            for (int i{}; i < next_move.size(); i++)
+                            {
+                                if (i <= 1)
+                                {
+                                    start_square += next_move[i];
+                                }
+                                else if (i <= 3)
+                                {
+                                    destination_square += next_move[i];
+                                }
+                                else
+                                {
+                                    promotion_piece += next_move[i];
+                                }
+                            }
+
+                            engine::MovesInfo pseudo_legal_moves{ board.pseudo_legal_move_gen(colour) };
+                            for (int i{}; i < pseudo_legal_moves.count; i++)
+                            {
+                                engine::Move& move{ pseudo_legal_moves.moves[i] };
+                                if (string_to_index[start_square] == move.start_square && string_to_index[destination_square] == move.destination_square && string_to_piece[promotion_piece] == move.promotion_piece)
+                                {
+                                    board.make_move(move);
+                                    colour = -colour;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+                else if (word == "go")
+                {
+                    int default_depth{ 7 };
+                    std::string sub_word{};
+
+                    while (input_stream >> sub_word)
+                    {
+                        if (sub_word == "depth")
+                        {
+                            input_stream >> default_depth;
+                        }
+
+                        // And we'll also have to add other things here for time management
+                    }
+
+                    engine::BestMove best_move{ search.get_best_move(default_depth, colour) };
+                    std::string best_move_string
+                    {
+                        index_to_string[best_move.move.start_square] 
+                        + index_to_string[best_move.move.destination_square]
+                    };
+
+                    if (best_move.move.promotion_piece != 0)
+                    {
+                        best_move_string += piece_to_string[best_move.move.promotion_piece];
+                    }
+
+                    std::cout << "bestmove " << best_move_string << std::endl;
+                    std::cout << std::flush;
+                }
+                else if (word == "quit")
+                {
+                    break;
+                }
             }
         }
     }
